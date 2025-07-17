d5782c4dcfe0b5a2a50768ba1442ddc7ede88024
Dmitrii Tomin <dimitrii.tomin@gmail.com>
2025-07-17 14:04:38 +0200

    bug fixes

--- image_segmentation/segment.py
+++ image_segmentation/segment.py
@@ -22,6 +22,7 @@ class Processing:
         h, w = self._pic.shape
         averages = 0
         prev_max = 0
+        maximum = 0
 
         tl = self._pic[:size, :size]
         tr = self._pic[:size, w - size :]
@@ -59,15 +60,15 @@ class Processing:
         bottom_right = pic[h2:, w2:]
         return [top_left, top_right, bottom_left, bottom_right]
 
-    def check(self, mean_threshold, max_threshold, skip, first_layer_list, n):
+    def check(self, mean_threshold, max_threshold, skip, layers_list, n):
         """Recursively segment the image based on the threshold."""
 
         # if the current depth exceeds the maximum depth, return
         if n >= self._depth:
             return
         # go through each image in the current layer
-        for tile in first_layer_list[n]:
-            # divide
+        for tile in layers_list[n]:
+            # Divide into quadrants
             segments = self.pic_divider(tile)
             # check the density of each segment
             for sec in segments:
@@ -75,32 +76,32 @@ class Processing:
                 if n >= skip and (
                     density <= mean_threshold or sec.max() <= max_threshold
                 ):
+                    sec.fill(np.nan)  # set to NaN if below threshold
+
+                    # or for colored visualization:
                     # sec *= 0  # set to zero if below threshold
                     # sec += (n + 1) * 400  # depending on the depth add value
-                    # or
-                    sec.fill(np.nan)  # set to NaN if below threshold
                 else:
-                    # if the density is above the threshold,
                     # add the segment to the next layer
-                    first_layer_list[n + 1].append(sec)
+                    layers_list[n + 1].append(sec)
 
         # Recursively check the next depth level
-        self.check(mean_threshold, max_threshold, skip, first_layer_list, n + 1)
+        self.check(mean_threshold, max_threshold, skip, layers_list, n + 1)
 
 
-class SegmentApp:
+class Segment:
     """Encapsulates image segmentation using recursive quadrant-based filtering."""
 
-    def __init__(self, initial_img, depth, skip, size, sigma, thresh_mult):
+    def __init__(self, initial_img, depth, skip, sigma, thresh_win, thresh_mult):
         """Initialize the SegmentApp with parameters."""
         self._initial_img = initial_img
         self._depth = depth
         self._skip = skip
-        self._size = size
+        self._thresh_win = thresh_win
         self._sigma = sigma
         self._thresh_mult = thresh_mult
 
-    def start_segment_app(self):
+    def start_segment(self):
         """Start the segmentation application."""
 
         original_img = self._initial_img.copy()
@@ -126,26 +127,24 @@ class SegmentApp:
 
         # Initialize a list to hold the segmented images at each depth
         # each element of the list corresponds to a depth level
-        first_layer_list = [[] for _ in range(self._depth + 1)]
-        first_layer_list[0].append(pic)  # Add the initial image to the first layer list
+        layers_list = [[] for _ in range(self._depth + 1)]
+        layers_list[0].append(pic)  # Add the initial image to the first layer list
 
-        processed = Processing(pic, self._depth)  # Initialize the Processing class
+        processed = Processing(pic, self._depth)
 
         # initialization_time = time.time() - start
 
         """Segment the image"""
         ################################################################################
         # Calculate the thresholds based on the border of the image
-        thresholds = processed.calculate_threshold(self._size, self._thresh_mult)
-        mean_threshold = thresholds[0]  # Get the mean threshold value
-        max_threshold = thresholds[1]  # Get the maximum threshold value
+        thresholds = processed.calculate_threshold(self._thresh_win, self._thresh_mult)
+        mean_threshold = thresholds[0]
+        max_threshold = thresholds[1]
 
         # threshold_time = time.time() - start - initialization_time
 
         # Start the segmentation process
-        processed.check(
-            mean_threshold, max_threshold, self._skip, first_layer_list, n=0
-        )
+        processed.check(mean_threshold, max_threshold, self._skip, layers_list, n=0)
         ################################################################################
 
         # segmentation_time = time.time() - start - threshold_time - initialization_time
@@ -165,9 +164,9 @@ class SegmentApp:
         original_img[mask] = np.nan  # Set NaN values in the original image
 
         processed_img = original_img
-        # processed_img = pic # Use if segments are not set to NaN
+        # processed_img = pic # Use for colored output
 
-        return {"blurred": blurred_img, "processed": processed_img}
+        return processed_img, blurred_img
 
 
 def plot_image(image, title="Image", xlabel="x", ylabel="y"):
@@ -184,9 +183,9 @@ def plot_image(image, title="Image", xlabel="x", ylabel="y"):
 if __name__ == "__main__":
     depth = 10  # Maximum depth of segmentation
     skip = 0  # Skip depths for segmentation
-    size = 100  # Size of the boxes for threshold calculation
     sigma = 2  # Sigma for Gaussian filter
-    threshold_multiplier = 1.5  # Threshold multiplier
+    thresh_win = 100  # Size of the boxes for threshold calculation
+    thresh_mult = 1.5  # Threshold multiplier
 
     """Load the beam and background images and process them"""
     ####################################################################################
@@ -197,7 +196,7 @@ if __name__ == "__main__":
 
     # subtract the background from the beam
     # and add a constant to avoid negative values
-    initial_img = beam.astype(float) - back.astype(float)  # [200:1800, 400:1750]TBD
+    initial_img = beam.astype(float) - back.astype(float)
     initial_img[initial_img < 0] = 0
     ####################################################################################
 
@@ -205,13 +204,11 @@ if __name__ == "__main__":
     # start = time.time()
 
     # Create an instance of SegmentApp with the initial image and parameters
-    app = SegmentApp(initial_img, depth, skip, size, sigma, threshold_multiplier)
+    app = Segment(initial_img, depth, skip, sigma, thresh_win, thresh_mult)
 
-    results = app.start_segment_app()
-    blurred_img = results["blurred"]  # Get the blurred image
-    processed_img = results["processed"]  # Get the processed image
+    processed_img, blurred_img = app.start_segment()
 
     # Plotting the images
-    # plot_image(initial_img, title="Image", xlabel="x", ylabel="y")
-    # plot_image(blurred_img, title="Image", xlabel="x", ylabel="y")
-    # plot_image(processed_img, title="Image", xlabel="x", ylabel="y")
+    plot_image(initial_img, title="Image", xlabel="x", ylabel="y")
+    plot_image(blurred_img, title="Image", xlabel="x", ylabel="y")
+    plot_image(processed_img, title="Image", xlabel="x", ylabel="y")
